c7d9a2c2f34877b3b3de60bbd36d149b3602922d
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2020-06-03 17:17:33 +0200

    Add basic MKS 937 simulator

---
+++ vazio/simulator/mks.py
@@ -0,0 +1,76 @@
+"""
+MKS simulator helper classes
+
+To create a MKS device use the following configuration as
+a starting point:
+
+.. code-block:: yaml
+
+    devices:
+    - class: MKS
+      package: vazio.simulator.mks
+      transports:
+      - type: serial
+        url: /tmp/mks00
+"""
+
+import random
+
+from sinstruments.simulator import BaseDevice
+
+
+def funiform(a, b):
+    return lambda: '{:7.1E}'.format(random.uniform(a, b))
+
+
+def iuniform(a, b):
+    return lambda: '{:05d}'.format(random.randint(a, b))
+
+
+state = {
+    'P1': funiform(5e-9, 9e-7),
+    'P2': funiform(5e-9, 9e-3),
+    'P3': funiform(5e-9, 9e-7),
+    'P4': funiform(5e-9, 9e-3),
+    'P5': funiform(5e-9, 9e-7),
+    'C1': funiform(5e-9, 9e-3),
+    'C2': funiform(5e-9, 9e-7),
+    'C3': funiform(5e-9, 9e-3),
+    'C4': funiform(5e-9, 9e-7),
+    'C5': funiform(5e-9, 9e-3),
+    'GAUGES': 'CvPrCv',
+    'PRO1': '4.1E-9',
+    'PRO2': '5.1E-9',
+    'PRO3': '6.1E-9',
+    'PRO4': '7.1E-9',
+    'PRO5': '8.1E-9',
+    'RELAYS': 'rly11011',
+    'RLY1': '4.7E-9',
+    'RLY2': '5.7E-9',
+    'RLY3': '6.7E-9',
+    'RLY4': '7.7E-9',
+    'RLY5': '8.7E-9',
+    'VER': '2.59,6.17',
+    'UNIT': 'mbar',
+    'ECC1': 'OK',
+    'ECC2': 'PROTECT!',
+    'ECC4': 'PS_FAULT!',
+    'XCC1': 'OK',
+    'XCC2': 'OK',
+    'XCC4': 'OK',
+}
+
+
+class MKS(BaseDevice):
+
+    DEFAULT_NEWLINE = b"\r"
+
+    def handle_line(self, line):
+        self._log.info("processing line %r", line)
+        line = line.decode()
+        data = state[line]
+        if callable(data):
+            data = data()
+        reply = data.encode() + b'\r'
+        self._log.debug("reply %r", reply)
+        return reply
